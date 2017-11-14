ed8d7c5a1699c1e4f0450e98adc956815c496ce0
Gatham M <15co118.gautham@nitk.edu.in>
2017-11-14 15:49:37 +0530

    minor changes

--- iping.cc
+++ iping.cc
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <cassert>
+#include "ns3/boolean.h"
+#include "ns3/uinteger.h"
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/csma-module.h"
@@ -18,10 +20,22 @@ int main (int argc, char *argv[])
 {
 	NS_LOG_INFO ("iPing - User friendly ping");
 
-	string userArg ("-v");  //By defaut, output lines will be produced.
+	uint32_t count = 0;
+	uint32_t ttl = 0;
+	uint32_t interval = 1;
+	uint32_t deadline = 15;
+	uint32_t packetsize ;
+	bool verbose = false;
+	bool quiet = false;
 
 	CommandLine cmd;
-	cmd.AddValue("userArg", "Pass arguments in this format:[-qv] [-c count] [-i interval] [-t ttl] [-w deadline]", userArg);
+  	cmd.AddValue("c", "Pass arguments in this format:[-qv] [-c count] [-i interval] [-t ttl] [-w deadline]", count);
+  	cmd.AddValue("i", "Pass arguments in this format:[-qv] [-c count] [-i interval] [-t ttl] [-w deadline]", interval);
+  	cmd.AddValue("t", "Pass arguments in this format:[-qv] [-c count] [-i interval] [-t ttl] [-w deadline]", ttl);
+  	cmd.AddValue("w", "Pass arguments in this format:[-qv] [-c count] [-i interval] [-t ttl] [-w deadline]", deadline);
+  	cmd.AddValue("s", "Pass arguments in this format:[-qv] [-c count] [-i interval] [-t ttl] [-w deadline]", packetsize);
+  	cmd.AddValue("v", "Pass arguments in this format:[-qv] [-c count] [-i interval] [-t ttl] [-w deadline]", verbose);
+  	cmd.AddValue("q", "Pass arguments in this format:[-qv] [-c count] [-i interval] [-t ttl] [-w deadline]", quiet);
   	cmd.Parse (argc, argv);
 
 	NodeContainer c;
@@ -41,16 +55,23 @@ int main (int argc, char *argv[])
 	Ipv4InterfaceContainer interfaces = address.Assign (devices);
 
 
-	ApplicationContainer app;
+	NS_LOG_INFO ("Create V4Ping Appliation");
+	Ptr<V4Ping> app = CreateObject<V4Ping> ();
 
-	V4PingHelper ping = V4PingHelper (interfaces.GetAddress(1));
+  	Config::SetDefault ("ns3::V4Ping::Size",   UintegerValue (70));
+	app->SetAttribute ("Remote", Ipv4AddressValue (interfaces.GetAddress(1)));
+	app->SetAttribute ("Verbose", BooleanValue (verbose) );
+	app->SetAttribute ("Count", UintegerValue (count));
+	app->SetAttribute ("Ttl", UintegerValue (ttl));     
+	app->SetAttribute ("Quiet", BooleanValue (quiet) );     
+	app->SetAttribute ("Interval", TimeValue (Seconds (interval)) );
+	//app->SetAttribute ("Size", UintegerValue (packetsize));
 
-	app = ping.Install (c.Get(0), userArg);
-	//app = ping.Install (c.Get(0));
 
-	//app.Start (Seconds (1.0));       //These are not required as the starttime and stoptime are being set in v4ping-helper.cc***
-	//app.Stop (Seconds (10.0));	   //However, if it is included here, it will just override those values***
-
+	c.Get(0)->AddApplication (app);
+	app->SetStartTime (Seconds (1.0));
+	app->SetStopTime (Seconds (1.0 + deadline));
+	
 	Packet::EnablePrinting ();
 
 	pointToPoint.EnablePcapAll("Example");
